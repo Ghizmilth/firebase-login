a5e54f1aeff9d588218d271a2655e5786a6fff30
Ghizmilth <hidanerferrer@gmail.com>
2018-11-05 15:27:05 -0800

    added Google signin capabilities

--- src/Authen.js
+++ src/Authen.js
@@ -6,7 +6,7 @@ var config = {
   authDomain: "fir-78979.firebaseapp.com",
   databaseURL: "https://fir-78979.firebaseio.com",
   projectId: "fir-78979",
-  storageBucket: "",
+  storageBucket: "fir-78979.appspot.com",
   messagingSenderId: "561194369947"
 };
 firebase.initializeApp(config);
@@ -61,6 +61,28 @@ class Authen extends Component {
     lout.classList.add("hide");
   }
 
+  google() {
+    console.log("I am a google method");
+    var provider = new firebase.auth.GoogleAuthProvider();
+    var promise = firebase.auth().signInWithPopup(provider);
+
+    promise.then(result => {
+      var user = result.user;
+      console.log(result);
+      firebase
+        .database()
+        .ref("users/" + user.uid)
+        .set({
+          email: user.email,
+          name: user.displayName
+        });
+    });
+    promise.catch(e => {
+      var msg = e.message;
+      console.log(msg);
+    });
+  }
+
   constructor(props) {
     super(props);
 
@@ -71,6 +93,7 @@ class Authen extends Component {
     this.login = this.login.bind(this);
     this.signup = this.signup.bind(this);
     this.logout = this.logout.bind(this);
+    this.google = this.google.bind(this);
   }
 
   render() {
@@ -95,6 +118,10 @@ class Authen extends Component {
         <button onClick={this.signup}>Sign Up</button>
         <button onClick={this.logout} id="logout" className="hide">
           Log Out
+        </button>
+        <br />
+        <button onClick={this.google} id="google" className="google">
+          Sign in with Google
         </button>
       </div>
     );
